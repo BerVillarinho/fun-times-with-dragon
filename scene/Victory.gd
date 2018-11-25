58d7b3a73ec0168ed775780ceba3f32591d08419
Bernardo Villarinho <bervillarinho@gmail.com>
2018-11-25 13:55:59 -0200

    What: Fixed some bugs that came with last commit
    Why: to make the game finished

--- scene/Victory.gd
+++ scene/Victory.gd
@@ -2,11 +2,12 @@ extends Control
 
 # class member variables go here, for example:
 # var a = 2
-var unblock = false
+var block = true
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
+	$Timer.start()
 	Tada.play()
 
 #func _process(delta):
@@ -15,13 +16,14 @@ func _ready():
 #	pass
 
 func _input(event):
-	var is_event_press = event is InputEventKey
-	var is_other_press =  event is InputEventMouseButton or event is InputEventJoypadButton
-	if (is_event_press and not event.echo) or is_other_press and event.pressed and $Timer.is_stopped() and unblock:
-		Confirm.play()
-		get_tree().paused = false
-		get_tree().change_scene("res://scene/menu.tscn")
-		queue_free()
+	if !block:
+		var is_event_press = event is InputEventKey
+		var is_other_press =  event is InputEventMouseButton or event is InputEventJoypadButton
+		if (is_event_press and not event.echo) or is_other_press and event.pressed:
+			Confirm.play()
+			get_tree().paused = false
+			get_tree().change_scene("res://scene/menu.tscn")
+			queue_free()
 
 func _on_Timer_timeout():
-	unblock = true
+	block = false
